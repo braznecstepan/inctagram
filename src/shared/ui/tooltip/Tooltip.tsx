464d8d42262757d78185a509f4f059e9d8f991a1
Dmitriy Storozhuk <gelendzhik94@yandex.ru>
2025-08-08 22:34:38 +0300

    SCRUM-14 feat: add header, tooltip, scrollbar

--- src/shared/ui/tooltip/Tooltip.tsx
+++ src/shared/ui/tooltip/Tooltip.tsx
@@ -1,106 +1,105 @@
 import { ComponentProps, ReactNode, useState } from 'react'
 
-
 import * as TooltipRadix from '@radix-ui/react-tooltip'
 import { clsx } from 'clsx'
 import { AnimatePresence, motion } from 'framer-motion'
 
 import s from './Tooltip.module.scss'
-import {OutlineBell} from '@/shared/ui/icons';
+import { OutlineBell } from '@/shared/ui/icons'
 
 type CommonProps = {
-    children: ReactNode
-    contentClassName?: string
-    /** Prevents tooltip content from remaining open when hovering */
-    disableHoverableContent?: boolean
-    side?: 'bottom' | 'left' | 'right' | 'top'
+  children: ReactNode
+  contentClassName?: string
+  /** Prevents tooltip content from remaining open when hovering */
+  disableHoverableContent?: boolean
+  side?: 'bottom' | 'left' | 'right' | 'top'
 } & ComponentProps<'div'>
 
 type ConditionalProps =
-    | {
-    component?: ReactNode
-    icon?: never
-}
-    | {
-    component?: never
-    icon?: ReactNode
-}
+  | {
+      component?: ReactNode
+      icon?: never
+    }
+  | {
+      component?: never
+      icon?: ReactNode
+    }
 
 export type TooltipProps = CommonProps & ConditionalProps
 const variants = {
-    active: {
-        opacity: 1,
-        y: 'var(--radix-tooltip-content-transform-origin)',
-    },
-    inactive: { opacity: 0, transition: { duration: 0.3 }, y: -40 },
+  active: {
+    opacity: 1,
+    y: 'var(--radix-tooltip-content-transform-origin)',
+  },
+  inactive: { opacity: 0, transition: { duration: 0.3 }, y: -40 },
 }
 
 const DELAY_DURATION = 200
 
 export const motionProps = { animate: 'active', exit: 'inactive', initial: 'inactive', variants }
 export const Tooltip = ({
-                            children,
-                            component,
-                            contentClassName,
-                            disableHoverableContent,
-                            icon,
-                            side = 'top',
-                            ...props
-                        }: TooltipProps) => {
-    const [open, setOpen] = useState(false)
+  children,
+  component,
+  contentClassName,
+  disableHoverableContent,
+  icon,
+  side = 'top',
+  ...props
+}: TooltipProps) => {
+  const [open, setOpen] = useState(false)
 
-    const classNames = {
-        arrow: s.arrow,
-        arrowBox: s.arrowBox,
-        content: clsx(s.content, contentClassName),
-        iconButton: s.iconButton,
-        infoIcon: s.infoIcon,
-    }
+  const classNames = {
+    arrow: s.arrow,
+    arrowBox: s.arrowBox,
+    content: clsx(s.content, contentClassName),
+    iconButton: s.iconButton,
+    infoIcon: s.infoIcon,
+  }
 
-    let tooltipTrigger: ReactNode
+  let tooltipTrigger: ReactNode
 
-    if (component) {
-        tooltipTrigger = <span>{component}</span>
-    } else {
-        tooltipTrigger = (
-            <button className={classNames.iconButton} type={'button'}>
-                {icon ?? (
-                    <span className={s.infoIcon}>
-            <OutlineBell/>
+  if (component) {
+    tooltipTrigger = <span>{component}</span>
+  } else {
+    tooltipTrigger = (
+      <button className={classNames.iconButton} type={'button'}>
+        {icon ?? (
+          <span className={s.infoIcon}>
+            <OutlineBell />
           </span>
-                )}
-            </button>
-        )
-    }
-
-    return (
-        <TooltipRadix.Provider delayDuration={DELAY_DURATION} {...props}>
-            <TooltipRadix.Root
-                disableHoverableContent={disableHoverableContent}
-                onOpenChange={setOpen}
-                open={open}
-            >
-                <TooltipRadix.Trigger asChild>{tooltipTrigger}</TooltipRadix.Trigger>
-                <AnimatePresence>
-                    {open && (
-                        <TooltipRadix.Portal forceMount>
-                            <TooltipRadix.Content
-                                asChild
-                                className={classNames.content}
-                                side={side}
-                                sideOffset={4}
-                            >
-                                <motion.div {...motionProps}>
-                                    {children}
-                                    <TooltipRadix.Arrow asChild className={classNames.arrowBox}>
-                                        <div className={classNames.arrow} />
-                                    </TooltipRadix.Arrow>
-                                </motion.div>
-                            </TooltipRadix.Content>
-                        </TooltipRadix.Portal>
-                    )}
-                </AnimatePresence>
-            </TooltipRadix.Root>
-        </TooltipRadix.Provider>
+        )}
+      </button>
     )
+  }
+
+  return (
+    <TooltipRadix.Provider delayDuration={DELAY_DURATION} {...props}>
+      <TooltipRadix.Root
+        disableHoverableContent={disableHoverableContent}
+        onOpenChange={setOpen}
+        open={open}
+      >
+        <TooltipRadix.Trigger asChild>{tooltipTrigger}</TooltipRadix.Trigger>
+        <AnimatePresence>
+          {open && (
+            <TooltipRadix.Portal forceMount>
+              <TooltipRadix.Content
+                asChild
+                className={classNames.content}
+                side={side}
+                sideOffset={4}
+              >
+                <motion.div {...motionProps}>
+                  {children}
+                  <TooltipRadix.Arrow asChild className={classNames.arrowBox}>
+                    <div className={classNames.arrow} />
+                  </TooltipRadix.Arrow>
+                </motion.div>
+              </TooltipRadix.Content>
+            </TooltipRadix.Portal>
+          )}
+        </AnimatePresence>
+      </TooltipRadix.Root>
+    </TooltipRadix.Provider>
+  )
 }
