2f3cc24c3b2a74de600482fa929b0b9b9c7fed29
Dmitriy Storozhuk <gelendzhik94@yandex.ru>
2025-08-08 23:08:33 +0300

    SCRUM-14 feat: add header, tooltip, scrollbar

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { useDispatch, useSelector, useStore } from 'react-redux'
 import { baseApi, baseReducer, baseSlice } from '@/shared'
-import process from 'process'
 
 export const makeStore = () =>
   configureStore({
@@ -20,10 +19,3 @@ export type AppDispatch = AppStore['dispatch']
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
 export const useAppSelector = useSelector.withTypes<AppState>()
 export const useAppStore = useStore.withTypes<AppStore>()
-
-const store = makeStore()
-
-if (process.env.NODE_ENV !== 'development') {
-  // @ts-ignore
-  window.store = store
-}
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -6,17 +6,18 @@ import { useAppSelector } from '@/app/store'
 import { selectIsLoggedIn, selectNotificationStatus } from '@/shared/api/base-slice'
 import { Tooltip } from '@/shared/ui/tooltip/Tooltip'
 import { Scrollbar } from '@/shared/ui/scrollbar/Scrollbar'
-import {useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation'
 
 export const Header = () => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
   const notificationStatus = useAppSelector(selectNotificationStatus)
   const router = useRouter()
+
   return (
     <header className={s.header}>
       <Container>
         <div className={s.headerWrapper}>
-          <button onClick={()=>router.push("/")} className={s.logo}>
+          <button type={'button'} onClick={() => router.push('/')} className={s.logo}>
             <h1>Inctagram</h1>
           </button>
           <div className={s.selectBox}>
@@ -51,9 +52,9 @@ export const Header = () => {
             </Tooltip>
             {/*Заглушка select, тут должен быть drop-down*/}
             <select defaultValue={'1'}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
+              <option value={'1'}>1</option>
+              <option value={'2'}>2</option>
+              <option value={'3'}>3</option>
             </select>
             {!isLoggedIn && (
               <div className={s.buttonWrapper}>
