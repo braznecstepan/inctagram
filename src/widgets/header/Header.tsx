ad5956ab1a9d18ddc08dcf8cf64a7dee90e12c79
Dmitriy Storozhuk <gelendzhik94@yandex.ru>
2025-08-08 22:34:38 +0300

    SCRUM-14 feat: add header, tooltip, scrollbar

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -6,23 +6,47 @@ import { useAppSelector } from '@/app/store'
 import { selectIsLoggedIn, selectNotificationStatus } from '@/shared/api/base-slice'
 import { Tooltip } from '@/shared/ui/tooltip/Tooltip'
 import { Scrollbar } from '@/shared/ui/scrollbar/Scrollbar'
+import {useRouter} from 'next/navigation';
 
 export const Header = () => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
   const notificationStatus = useAppSelector(selectNotificationStatus)
-
+  const router = useRouter()
   return (
     <header className={s.header}>
       <Container>
         <div className={s.headerWrapper}>
-          <h1 className={s.logo}>Inctagram</h1>
+          <button onClick={()=>router.push("/")} className={s.logo}>
+            <h1>Inctagram</h1>
+          </button>
           <div className={s.selectBox}>
             {isLoggedIn && notificationStatus && <OutlineBell />}
-            <Tooltip  side={'bottom'}>
-              <Scrollbar type={'scroll'}>
-                <ul>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum sit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum sitLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum sitLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum sitLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum sitLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum sitLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum sitLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum sit</p>
-                </ul>
+            <Tooltip side={'bottom'}>
+              <Scrollbar maxHeight={425} type={'scroll'}>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem!
+                  Animi architecto dolore dolores doloribus earum enim id illo, incidunt laudantium
+                  magni porro praesentium quaerat qui quia rerum sit.Lorem ipsum dolor sit amet,
+                  consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore
+                  dolores doloribus earum enim id illo, incidunt laudantium magni porro praesentium
+                  quaerat qui quia rerum sitLorem ipsum dolor sit amet, consectetur adipisicing
+                  elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores doloribus earum
+                  enim id illo, incidunt laudantium magni porro praesentium quaerat qui quia rerum
+                  sitLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, sunt,
+                  voluptatem! Animi architecto dolore dolores doloribus earum enim id illo, incidunt
+                  laudantium magni porro praesentium quaerat qui quia rerum sitLorem ipsum dolor sit
+                  amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto
+                  dolore dolores doloribus earum enim id illo, incidunt laudantium magni porro
+                  praesentium quaerat qui quia rerum sitLorem ipsum dolor sit amet, consectetur
+                  adipisicing elit. Ipsam, sunt, voluptatem! Animi architecto dolore dolores
+                  doloribus earum enim id illo, incidunt laudantium magni porro praesentium quaerat
+                  qui quia rerum sitLorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam,
+                  sunt, voluptatem! Animi architecto dolore dolores doloribus earum enim id illo,
+                  incidunt laudantium magni porro praesentium quaerat qui quia rerum sitLorem ipsum
+                  dolor sit amet, consectetur adipisicing elit. Ipsam, sunt, voluptatem! Animi
+                  architecto dolore dolores doloribus earum enim id illo, incidunt laudantium magni
+                  porro praesentium quaerat qui quia rerum sit
+                </p>
               </Scrollbar>
             </Tooltip>
             {/*Заглушка select, тут должен быть drop-down*/}
@@ -33,8 +57,10 @@ export const Header = () => {
             </select>
             {!isLoggedIn && (
               <div className={s.buttonWrapper}>
-                <Button variant={'outlined'}>Log in</Button>
-                <Button>Sign up</Button>
+                <Button onClick={() => router.push('/auth/sign-in')} variant={'outlined'}>
+                  Log in
+                </Button>
+                <Button onClick={() => router.push('/auth/sign-up')}>Sign up</Button>
               </div>
             )}
           </div>
