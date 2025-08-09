95c97b696f95e5e7a92501a075376151dc4b94a0
Nazar <ups14q@gmail.com>
2025-08-09 10:55:45 +0300

    feat: enhance DropDown component with new icons and improved styling

--- src/shared/ui/drop_down/DropDown.tsx
+++ src/shared/ui/drop_down/DropDown.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import clsx from 'clsx'
 import * as Select from '@radix-ui/react-select'
 
+import { ChevronDownIcon } from '../icons/ChevronDownIcon'
+import { ChevronUpIcon } from '../icons/ChevronUpIcon'
+
 import s from './DropDown.module.scss'
 
 export type DropDownOption = {
@@ -99,26 +102,3 @@ export const DropDown: React.FC<DropDownProps> = ({
     </div>
   )
 }
-
-// Icons
-const ChevronDownIcon = () => (
-  <svg width="15" height="15" viewBox="0 0 15 15" fill="none">
-    <path
-      d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z"
-      fill="currentColor"
-      fillRule="evenodd"
-      clipRule="evenodd"
-    />
-  </svg>
-)
-
-const ChevronUpIcon = () => (
-  <svg width="15" height="15" viewBox="0 0 15 15" fill="none">
-    <path
-      d="M3.13523 8.84197C3.3241 9.04343 3.64052 9.05363 3.84197 8.86477L7.5 5.43536L11.158 8.86477C11.3595 9.05363 11.6759 9.04343 11.8648 8.84197C12.0536 8.64051 12.0434 8.32409 11.842 8.13523L7.84197 4.38523C7.64964 4.20492 7.35036 4.20492 7.15803 4.38523L3.15803 8.13523C2.95657 8.32409 2.94637 8.64051 3.13523 8.84197Z"
-      fill="currentColor"
-      fillRule="evenodd"
-      clipRule="evenodd"
-    />
-  </svg>
-)
