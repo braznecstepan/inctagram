3165a246cbb0a31b1f236f9d049c86a87775c362
roma1524 <kulikr.js@mail.ru>
2025-08-11 17:58:18 +0300

    SCRUM-11 validationSchema is done

--- src/pages/auth/ui/sign_in/SignIn.tsx
+++ src/pages/auth/ui/sign_in/SignIn.tsx
@@ -1,18 +1,32 @@
 import { EyeOffOutline, EyeOutline, GithubSvgrepoCom31, GoogleSvgrepoCom1 } from '@/shared/ui/icons'
-import { FormEvent } from 'react'
 import { Button, Card, TextField } from '@/shared/ui'
 
 import s from './SignIn.module.scss'
 import Link from 'next/link'
 import { usePasswordMode } from '@/pages/auth/lib'
 import { AUTH_ROUTES } from '@/shared/lib/routes'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { signInSchema, signInType } from '@/pages/auth/model/validation'
 
 export function SignIn() {
   const { mode, toggleMode } = usePasswordMode()
 
-  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    alert('Sign In')
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<signInType>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: zodResolver(signInSchema),
+  })
+
+  const handleFormSubmit: SubmitHandler<signInType> = data => {
+    reset()
   }
 
   const classnames = {
@@ -43,9 +57,11 @@ export function SignIn() {
           </Link>
         </div>
 
-        <form className={classnames.form} onSubmit={handleFormSubmit}>
+        <form className={classnames.form} onSubmit={handleSubmit(handleFormSubmit)}>
           <TextField
             className={classnames.email}
+            {...register('email')}
+            errorMessage={errors.email && errors.email.message}
             placeholder={'it-incubator@gmail.com'}
             label={'Email'}
           />
@@ -53,6 +69,8 @@ export function SignIn() {
           <TextField
             className={classnames.password}
             type={mode ? 'password' : 'text'}
+            {...register('password')}
+            errorMessage={errors.password && errors.password.message}
             placeholder={'••••••••••'}
             label={'Password'}
             iconEnd={mode ? <EyeOffOutline /> : <EyeOutline />}
--- src/pages/auth/ui/sign_up/SignUp.tsx
+++ src/pages/auth/ui/sign_up/SignUp.tsx
@@ -1,20 +1,40 @@
 import { EyeOffOutline, EyeOutline, GithubSvgrepoCom31, GoogleSvgrepoCom1 } from '@/shared/ui/icons'
-import { FormEvent } from 'react'
 import { Button, Card, TextField } from '@/shared/ui'
 
 import s from './SignUp.module.scss'
 import Link from 'next/link'
 import { usePasswordMode } from '@/pages/auth/lib'
 import { AUTH_ROUTES } from '@/shared/lib/routes'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+import { Checkbox } from '@/shared/ui/checkbox/CheckBox'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { signUpSchema, signUpType } from '@/pages/auth/model/validation'
 
 export function SignUp() {
   const { mode: passwordMode, toggleMode: togglePasswordMode } = usePasswordMode()
   const { mode: passwordConfirmationMode, toggleMode: togglePasswordConfirmationMode } =
     usePasswordMode()
 
-  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    alert('Sign Up')
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<signUpType>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      isAgree: false,
+    },
+    resolver: zodResolver(signUpSchema),
+  })
+
+  const handleFormSubmit: SubmitHandler<signUpType> = data => {
+    // console.log(data)
+    reset()
   }
 
   const classnames = {
@@ -33,6 +53,7 @@ export function SignUp() {
     signUp: s.signUp,
     question: s.question,
     signIn: s.signIn,
+    isAgree: s.isAgree,
   }
 
   return (
@@ -49,43 +70,74 @@ export function SignUp() {
           </Link>
         </div>
 
-        <form className={classnames.form} onSubmit={handleFormSubmit}>
-          <TextField className={classnames.username} placeholder={'Dimych'} label={'Username'} />
+        <form className={classnames.form} onSubmit={handleSubmit(handleFormSubmit)}>
+          <TextField
+            className={classnames.username}
+            placeholder={'Dimych'}
+            label={'Username'}
+            errorMessage={errors.name && errors.name.message}
+            {...register('name')}
+            required={true}
+          />
 
           <TextField
+            errorMessage={errors.email && errors.email.message}
             className={classnames.email}
+            {...register('email')}
             placeholder={'it-incubator@gmail.com'}
             label={'Email'}
+            required={true}
           />
 
           <TextField
             className={classnames.password}
+            errorMessage={errors.password && errors.password.message}
             type={passwordMode ? 'password' : 'text'}
+            {...register('password')}
             placeholder={'••••••••••'}
             label={'Password'}
             iconEnd={passwordMode ? <EyeOffOutline /> : <EyeOutline />}
             onEndIconClick={togglePasswordMode}
+            required={true}
           />
 
           <TextField
             className={classnames.passwordConfirmation}
+            errorMessage={errors.passwordConfirmation && errors.passwordConfirmation.message}
             type={passwordConfirmationMode ? 'password' : 'text'}
+            {...register('passwordConfirmation')}
             placeholder={'••••••••••'}
             label={'Password confirmation'}
             iconEnd={passwordConfirmationMode ? <EyeOffOutline /> : <EyeOutline />}
             onEndIconClick={togglePasswordConfirmationMode}
+            required={true}
           />
 
-          <span className={classnames.compliance}>
-            I agree to the{' '}
-            <Button variant={'text'} className={classnames.termsOfServices} asChild>
-              <Link href={AUTH_ROUTES.TERMS_OF_SERVICE}>Terms of Service</Link>
-            </Button>{' '}
-            and{' '}
-            <Button variant={'text'} className={classnames.privacyPolicy} asChild>
-              <Link href={AUTH_ROUTES.PRIVACY_POLICY}>Privacy Policy</Link>
-            </Button>
-          </span>
+          <Controller
+            control={control}
+            name="isAgree"
+            render={({ field }) => (
+              <div className={classnames.isAgree}>
+                <Checkbox
+                  onChange={e => field.onChange(e)}
+                  required={true}
+                  checked={field.value}
+                  label={
+                    <span className={classnames.compliance}>
+                      I agree to the{' '}
+                      <Button variant={'text'} className={classnames.termsOfServices} asChild>
+                        <Link href={AUTH_ROUTES.TERMS_OF_SERVICE}>Terms of Service</Link>
+                      </Button>{' '}
+                      and{' '}
+                      <Button variant={'text'} className={classnames.privacyPolicy} asChild>
+                        <Link href={AUTH_ROUTES.PRIVACY_POLICY}>Privacy Policy</Link>
+                      </Button>
+                    </span>
+                  }
+                />
+              </div>
+            )}
+          />
 
           <Button variant={'primary'} className={classnames.signUp} type={'submit'}>
             Sign Up
