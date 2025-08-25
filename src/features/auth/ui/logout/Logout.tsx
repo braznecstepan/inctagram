3f160afe3f3c2eddbbce1271f16e778383886b22
roma1524 <kulikr.js@mail.ru>
2025-08-25 23:33:31 +0300

    FIX_SCRUM-33:feat logout component

--- src/features/auth/ui/logout/Logout.tsx
+++ src/features/auth/ui/logout/Logout.tsx
@@ -1,25 +1,28 @@
 'use client'
 import { useAppDispatch, useAppSelector } from '@/shared/lib/hooks'
 import { changeIsLoggedIn, selectIsLoggedIn } from '@/shared/api/base-slice'
-import { useLogoutMutation } from '@/entities/auth/api'
+import { useLogoutMutation, useMeQuery } from '@/entities/auth/api'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/shared/ui'
 
 export const Logout = () => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
   const [logout] = useLogoutMutation()
+  const { data: userData } = useMeQuery()
   const router = useRouter()
   const dispatch = useAppDispatch()
 
   const confirmHandler = () => {
-    const isConfirmMessage = confirm(`Are you really want to log out of your account ${'email'}?`)
+    const isConfirmMessage = confirm(
+      `Are you really want to log out of your account ${userData?.email}?`
+    )
     if (!isConfirmMessage) {
       return
     }
     logout().then(() => {
       dispatch(changeIsLoggedIn({ isLoggedIn: false }))
       localStorage.removeItem('token')
-      return router.push('/')
+      return router.push('/auth/sign-in')
     })
   }
 
