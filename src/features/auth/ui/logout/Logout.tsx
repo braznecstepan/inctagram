39878c15fa30f06c2499d9bf7459946ea70eeb55
roma1524 <kulikr.js@mail.ru>
2025-08-25 22:45:04 +0300

    SCRUM-33:feat logout component

--- src/features/auth/ui/logout/Logout.tsx
+++ src/features/auth/ui/logout/Logout.tsx
@@ -1,3 +1,35 @@
+'use client'
+import { useAppDispatch, useAppSelector } from '@/shared/lib/hooks'
+import { changeIsLoggedIn, selectIsLoggedIn } from '@/shared/api/base-slice'
+import { useLogoutMutation } from '@/entities/auth/api'
+import { useRouter } from 'next/navigation'
+import { Button } from '@/shared/ui'
+
 export const Logout = () => {
-  return <h1>Logout Component</h1>
+  const isLoggedIn = useAppSelector(selectIsLoggedIn)
+  const [logout] = useLogoutMutation()
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+
+  const confirmHandler = () => {
+    const isConfirmMessage = confirm(`Are you really want to log out of your account ${'email'}?`)
+    if (!isConfirmMessage) {
+      return
+    }
+    logout().then(() => {
+      dispatch(changeIsLoggedIn({ isLoggedIn: false }))
+      localStorage.removeItem('token')
+      return router.push('/')
+    })
+  }
+
+  return (
+    <>
+      {isLoggedIn && (
+        <Button variant="outlined" onClick={confirmHandler}>
+          Log out
+        </Button>
+      )}
+    </>
+  )
 }
