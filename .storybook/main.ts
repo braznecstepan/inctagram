5fbc82c8f32c3081eff410c7bc0b303c895d99bb
Dmitriy Storozhuk <gelendzhik94@yandex.ru>
2025-08-31 17:54:24 +0300

    feat(add-storybook): В проект добавлен storybook.

---
+++ .storybook/main.ts
@@ -0,0 +1,30 @@
+import type { StorybookConfig } from '@storybook/nextjs-vite'
+import { dirname, join } from 'path'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
+  ],
+  framework: {
+    name: '@storybook/nextjs-vite',
+    options: {},
+  },
+  staticDirs: ['..\\public'],
+  async viteFinal(config) {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve?.alias,
+          src: join(dirname(__dirname), 'src'),
+        },
+      },
+    }
+  },
+}
+export default config
