6ac3c61ea3f3f111bbf9ffdd02fe82a0c2fd124b
sergey-mikhovich <sergey.mikhovich98@gmail.com>
2025-09-03 11:16:23 +0300

    correct redirects on home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,56 +1,22 @@
 'use client'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useAppDispatch, useAppSelector } from '@/shared/lib/hooks'
+
+import { useRouter } from 'next/navigation'
+import { useAppSelector } from '@/shared/lib/hooks'
 import { selectIsLoggedIn } from '@/shared/api/base-slice'
-import { AUTH_ROUTES } from '@/shared/lib/routes'
+import { AUTH_ROUTES, PROFILE_ROUTES } from '@/shared/lib/routes'
 import { useEffect } from 'react'
-import { useRegistrationConfirmationMutation } from '@/entities/auth/api'
-import { handleNetworkError } from '@/shared/lib'
 
 export function Home() {
-  return null
-  // return (
-  //   <Suspense fallback={<h1>Loading...</h1>}>
-  //     <PrivateHome />
-  //   </Suspense>
-  // )
-}
-
-const PrivateHome = () => {
   const router = useRouter()
-  const searchParams = useSearchParams()
-  const dispatch = useAppDispatch()
-  const [registrationConfirmation] = useRegistrationConfirmationMutation()
-
-  const code = searchParams?.get('code')
-
   const isLoggedIn = useAppSelector(selectIsLoggedIn)
 
-  const handleConfirmationRegistration = async (confirmationCode: string) => {
-    try {
-      await registrationConfirmation({ confirmationCode }).unwrap()
-      router.push(AUTH_ROUTES.CONFIRMED_EMAIL)
-    } catch (error: unknown) {
-      handleNetworkError({ error, dispatch })
-      router.push(AUTH_ROUTES.EXPIRED_LINK)
-    }
-  }
-
   useEffect(() => {
-    if (!isLoggedIn && !code) {
-      router.push(AUTH_ROUTES.SIGN_IN)
+    if (!isLoggedIn) {
+      router.replace(AUTH_ROUTES.SIGN_IN)
+    } else {
+      router.replace(PROFILE_ROUTES.SETTINGS)
     }
+  }, [isLoggedIn])
 
-    if (code) {
-      void handleConfirmationRegistration(code)
-    }
-  }, [isLoggedIn, code])
-
-  return (
-    <div>
-      <main>
-        <h1>Loading...</h1>
-      </main>
-    </div>
-  )
+  return null
 }
