1d8d592480f2451acca1befd83a2df718789b95b
roma1524 <kulikr.js@mail.ru>
2025-09-04 13:48:18 +0300

    create SidebarItem

---
+++ src/widgets/sidebar/ui/Sidebar/SidebarItem/SidebarItem.tsx
@@ -0,0 +1,22 @@
+import s from '@/widgets/sidebar/ui/Sidebar/Sidebar.module.scss'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { ReactNode } from 'react'
+
+type Props = {
+  title: string
+  href: string
+  children: ReactNode
+}
+
+export const SidebarItem = ({ title, href, children }: Props) => {
+  const pathname = usePathname()
+  const isActive = pathname === href
+
+  return (
+    <Link href={href} className={`${s.link} ${isActive ? s.active : ''}`} aria-label={title}>
+      {children}
+      {title}
+    </Link>
+  )
+}
