a28bde3be618e594304c5143c2b9ec90f92b2bfe
roma1524 <kulikr.js@mail.ru>
2025-09-09 22:59:22 +0300

    SCRUM87: It's for future use

--- src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
@@ -14,6 +14,19 @@ import { SidebarItem } from '@/widgets/sidebar'
 import { CreatePublication } from '@/pages/create_publication'
 
 export const Sidebar = () => {
+
+  const mainLinks = [
+    { title: 'Feed', href: '/feed', icon: <HomeOutline /> },
+    { title: 'My Profile', href: '/profile', icon: <PersonOutline /> },
+    { title: 'Messenger', href: '/messenger', icon: <MessageCircleOutline /> },
+    { title: 'Search', href: '/search', icon: <SearchOutline /> },
+  ]
+
+  const extraLinks = [
+    { title: 'Statistics', href: '/statistics', icon: <TrendingUpOutline /> },
+    { title: 'Favorites', href: '/favorites', icon: <BookmarkOutline /> },
+  ]
+
   return (
     <nav className={s.sidebar} aria-label="Main navigation">
       <div>
