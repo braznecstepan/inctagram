b7a32edad297db4c3a2397664f9ef7a2d63d6600
roma1524 <kulikr.js@mail.ru>
2025-09-09 22:31:29 +0300

    SCRUM87_feat: CreatePublication component

--- src/pages/create_publication/CreatePublication.tsx
+++ src/pages/create_publication/CreatePublication.tsx
@@ -1,7 +1,30 @@
-export function CreatePublication() {
+import {Button, Card, Modal} from '@/shared/ui'
+import { useState } from 'react'
+import s from './CreatePublication.module.scss'
+import {Image} from "@/shared/ui/icons";
+
+type Props = {
+  title: string
+  className: string
+}
+
+export function CreatePublication({ title, className }: Props) {
+  const [isShowModal, setShowModal] = useState<boolean>(false)
+
+  const toggleModal = () => setShowModal(prev => !prev)
+
   return (
     <>
-      <h1>Create Publication</h1>
+      <Button onClick={toggleModal} variant={'text'} className={className}>{title}</Button>
+        <Modal open={isShowModal} title={'Add Photo'} onClose={toggleModal}>
+            <div className={s.modal}>
+                <Card className={s.card} >
+                    <Image width={48} height={48}/>
+                </Card>
+                <Button className={s.select}>Select from Computer</Button>
+                <Button variant={"outlined"}>Open Draft</Button>
+            </div>
+        </Modal>
     </>
   )
 }
