833a197be7edd482eef8faa28f95e5e518ea87db
roma1524 <kulikr.js@mail.ru>
2025-09-09 22:33:06 +0300

    SCRUM87_fix: refactoring Sidebar & scss

--- src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar/Sidebar.tsx
@@ -11,54 +11,45 @@ import {
   TrendingUpOutline,
 } from '@/shared/ui/icons'
 import { SidebarItem } from '@/widgets/sidebar'
+import { CreatePublication } from '@/pages/create_publication'
 
 export const Sidebar = () => {
   return (
     <nav className={s.sidebar} aria-label="Main navigation">
-        <div>
-          <div className={s.main}>
-            <SidebarItem
-                title={'Feed'}
-                href={'/feed'}
-                startDecoration={<HomeOutline />} />
-            <SidebarItem
-                title={'Create'}
-                href={'/post'}
-                startDecoration={<PlusSquareOutline />} />
-            <SidebarItem
-              title={'My Profile'}
-              href={'/profile'}
-              startDecoration={<PersonOutline />}
-            />
-            <SidebarItem
-              title={'Messenger'}
-              href={'/messenger'}
-              startDecoration={<MessageCircleOutline />}
-            />
-            <SidebarItem
-                title={'Search'}
-                href={'/search'}
-                startDecoration={<SearchOutline />} />
+      <div>
+        <div className={s.main}>
+          <SidebarItem title={'Feed'} href={'/feed'} startDecoration={<HomeOutline />} />
+          <div className={s.create}>
+            <PlusSquareOutline />
+            <CreatePublication title={'Create'} className={s.createBtn}/>
           </div>
-
-          <div className={s.list}>
-            <SidebarItem
-              title={'Statistics'}
-              href={'/statistics'}
-              startDecoration={<TrendingUpOutline />}
-            />
-            <SidebarItem
-              title={'Favorites'}
-              href={'/favorites'}
-              startDecoration={<BookmarkOutline />}
-            />
-          </div>
+          <SidebarItem title={'My Profile'} href={'/profile'} startDecoration={<PersonOutline />} />
+          <SidebarItem
+            title={'Messenger'}
+            href={'/messenger'}
+            startDecoration={<MessageCircleOutline />}
+          />
+          <SidebarItem title={'Search'} href={'/search'} startDecoration={<SearchOutline />} />
         </div>
 
-        <div className={s.logout}>
-          <LogOutOutline aria-label="Log out" />
-          <Logout className={s.logoutBtn} />
+        <div className={s.list}>
+          <SidebarItem
+            title={'Statistics'}
+            href={'/statistics'}
+            startDecoration={<TrendingUpOutline />}
+          />
+          <SidebarItem
+            title={'Favorites'}
+            href={'/favorites'}
+            startDecoration={<BookmarkOutline />}
+          />
         </div>
+      </div>
+
+      <div className={s.logout}>
+        <LogOutOutline aria-label="Log out" />
+        <Logout className={s.logoutBtn} />
+      </div>
     </nav>
   )
 }
