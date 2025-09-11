60c2ca0bcade05f13c882cb4dd1a76d392f3ca6c
roma1524 <kulikr.js@mail.ru>
2025-09-11 15:54:52 +0300

    SCRUM87: create useLogout

---
+++ src/features/auth/lib/hooks/useLogout.ts
@@ -0,0 +1,26 @@
+import {authApi, useLogoutMutation, useMeQuery} from "@/entities/auth/api";
+import {useAppDispatch} from "@/shared/lib/hooks";
+import {handleNetworkError} from "@/shared/lib";
+
+export const useLogout = () => {
+    const [logout] = useLogoutMutation()
+    const { data: userData } = useMeQuery()
+    const dispatch = useAppDispatch()
+
+    const handleConfirmClick = async () => {
+        try {
+            await logout().unwrap()
+            dispatch(authApi.util.resetApiState())
+        } catch (error: unknown) {
+            const handle401Error = () => {
+                dispatch(authApi.util.resetApiState())
+            }
+
+            handleNetworkError({ error, dispatch, handle401Error })
+        }
+    }
+    return {
+        onLogout: handleConfirmClick,
+        userData
+    }
+}
